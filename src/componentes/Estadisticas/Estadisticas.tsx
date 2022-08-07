1a0a35bd4c9635e0dbb3666c6ce134aa80594f90
JuanBenitez2600 <juan_manuel.benitez@uao.edu.co>
2022-08-07 14:38:35 -0500

    actualizando estadisticas.tsx

---
+++ src/componentes/Estadisticas/Estadisticas.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Estadisticas() {
+  return (
+    <div>
+        <div>
+            
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
